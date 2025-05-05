8ca51a8b32e41cfb093456646e260094249e97ee
Milan Jovic <milan.jovic86@gmail.com>
2025-05-06 00:17:39 +0200

    Develop:  - Clean up the repo. Remove not used code.

--- feature-movies/src/test/java/android/template/feature/movies/MoviesViewModelTest.kt
+++ feature-movies/src/test/java/android/template/feature/movies/MoviesViewModelTest.kt
@@ -1,44 +1,12 @@
-package android.template.feature.mymodel.ui.mymodel
+package android.template.feature.movies
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.test.runTest
-import org.junit.Assert.assertEquals
-import org.junit.Test
-import android.template.core.data.MoviesRepository
-import android.template.core.data.remote.paging.MoviesPagingSource
-import android.template.feature.movies.MoviesViewModel
 
 /**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
+ * Unit test for the [MoviesViewModel].
  */
-@OptIn(ExperimentalCoroutinesApi::class) // TODO: Remove when stable
-class MoviesViewModelTest { // TODO: Implement tests
-
-    @Test
-    fun uiState_initiallyLoading() = runTest {
-        val viewModel = MoviesViewModel(FakeMoviesRepository())
-        assertEquals(viewModel.uiState.first(), MyModelUiState.Loading)
-    }
-
-    @Test
-    fun uiState_onItemSaved_isDisplayed() = runTest {
-        val viewModel = MyModelViewModel(FakeMoviesRepository())
-        assertEquals(viewModel.uiState.first(), MyModelUiState.Loading)
-    }
+@OptIn(ExperimentalCoroutinesApi::class)
+class MoviesViewModelTest {
+    // TODO: Implement tests
 }
 
-private class FakeMoviesRepository : MoviesRepository {
-
-    private val data = mutableListOf<String>()
-
-    override fun getMoviesPagingSource(
-        genreId: Int
-    ): MoviesPagingSource {
-        TODO("Not yet implemented")
-    }
-}
